dd007cc9f99d0cf1aacf919ab3722e1dbcdd7a76
TakaoWing <tt.wing2838783@gmail.com>
2017-04-06 03:15:17 +0900

    complete ArrowGenerator

--- Assets/Scripts/ArrowGenerator.cs
+++ Assets/Scripts/ArrowGenerator.cs
@@ -9,15 +9,15 @@ public class ArrowGenerator : MonoBehaviour {
 	float delta = 0;
 
 	void Start () {
-		
 	}
 
 	void Update () {
-		this.delta = Time.deltaTime;
-		if(this.delta > this.span){
+		this.delta += Time.deltaTime;
+
+		if(this.delta > this.span){ // span秒ごとに矢を作成
 			this.delta = 0;
-			GameObject go = Instantiate (arrowPrefab) as GameObject;
-			int px = Random.Range (-6, 7);
+			GameObject go = Instantiate (arrowPrefab) as GameObject; // GameObject型にキャスト
+			int px = Random.Range (-6, 7); // -6から7の乱数をpxに代入
 			go.transform.position = new Vector3 (px, 7, 0);
 		}
 	}
